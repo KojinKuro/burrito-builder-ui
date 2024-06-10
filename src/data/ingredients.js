ad5d7bd377943d6e8300091422469b2b9bf070e6
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-06-10 10:52:28 -0700

    feat: Allow calculation for cost on uncalculated cost

---
+++ src/data/ingredients.js
@@ -0,0 +1,16 @@
+import { createIngredient } from "../utils/ingredients";
+
+export const possibleIngredients = [
+  createIngredient("beans", 0.5),
+  createIngredient("steak", 5),
+  createIngredient("carnitas", 4),
+  createIngredient("sofritas", 1),
+  createIngredient("lettuce", 0.25),
+  createIngredient("queso fresco", 1),
+  createIngredient("pico de gallo", 0.25),
+  createIngredient("hot sauce", 0.25),
+  createIngredient("guacamole", 3),
+  createIngredient("jalapeno", 0.5),
+  createIngredient("cilantro", 0.25),
+  createIngredient("sour cream", 0.5),
+];
--- src/utils/ingredients.js
+++ src/utils/ingredients.js
@@ -1,3 +1,25 @@
 export function createIngredient(name, cost) {
   return { name, cost };
 }
+
+export function findIngredient(ingredientName, ingredients) {
+  const foundIngredient = ingredients.find(
+    (ingredient) => ingredient.name === ingredientName
+  );
+
+  if (!foundIngredient) return createIngredient(ingredientName, 0);
+  return foundIngredient;
+}
+
+export function findIngredients(ingredientNames, ingredients) {
+  return ingredientNames.map((ingredientName) =>
+    findIngredient(ingredientName, ingredients)
+  );
+}
+
+export function calculateTotalCost(ingredients) {
+  return ingredients.reduce(
+    (totalCost, ingredient) => totalCost + ingredient.cost,
+    0
+  );
+}
